cb8d2699bb63c17af5b2ee0679b6b65fcb20157f
Your Name <you@example.com>
2025-01-16 05:57:56 -0600

    Fixed import paths in AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,38 +1,30 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Layout from '../components/Layout/Layout';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import LearningCenter from '../pages/LearningCenter';
-import LocalStores from '../pages/LocalStores';
-import Community from '../pages/Community';
 import PlantGuide from '../pages/PlantGuide';
-import Tutorials from '../pages/Tutorials';
 import FarmingStyles from '../pages/FarmingStyles';
 import PlantIdentifier from '../pages/PlantIdentifier';
-import Events from '../pages/Events';
-import Mentors from '../pages/Mentors';
-import GrowingSeasons from '../pages/GrowingSeasons';
-import SoilGuide from '../pages/SoilGuide';
+import Tutorials from '../pages/Tutorials';
+import StoreLocator from '../pages/LocalStores/StoreLocator';
+import Events from '../pages/Community/Events';
+import Mentors from '../pages/Community/Mentors';
+import Login from '../pages/Auth/Login';
+import Register from '../pages/Auth/Register';
 
-const AppRoutes = () => {
+const AppRoutes: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/learning" element={<LearningCenter />} />
-          <Route path="/stores" element={<LocalStores />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/plants" element={<PlantGuide />} />
-          <Route path="/tutorials" element={<Tutorials />} />
-          <Route path="/farming-styles" element={<FarmingStyles />} />
-          <Route path="/plant-identifier" element={<PlantIdentifier />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/mentors" element={<Mentors />} />
-          <Route path="/growing-seasons" element={<GrowingSeasons />} />
-          <Route path="/soil-guide" element={<SoilGuide />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/plant-guide" element={<PlantGuide />} />
+      <Route path="/farming-styles" element={<FarmingStyles />} />
+      <Route path="/plant-identifier" element={<PlantIdentifier />} />
+      <Route path="/tutorials" element={<Tutorials />} />
+      <Route path="/store-locator" element={<StoreLocator />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/mentors" element={<Mentors />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 };
 
